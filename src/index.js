51b8c7fca353f96f28ad18417b6906ab610ca377
prashant <prashant9546289053@gmail.com>
2023-09-09 08:19:22 +0530

    commiting city repository

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const {PORT} = require('./configs/serverConfig')
+const CityRepository = require('./repository/city-repository')
 
 const setupAndStartServer = async () => {
     // create the express object
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
     const PORT = 3000;
-    app.listen(PORT , () =>{
-        console.log(`server started at ${PORT}`) 
+    app.listen(PORT , async() =>{
+        console.log(`server started at ${PORT}`) ;
+        const repo = new CityRepository();
+        repo.createCity({name:"New Delhi"});
     })
 }
 
